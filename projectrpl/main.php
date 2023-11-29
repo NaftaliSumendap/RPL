871a0167b56569db3d142926b049193271697e9d
NaftaliSumendap <132432392+NaftaliSumendap@users.noreply.github.com>
2023-11-29 08:54:35 +0800

    test#2

--- projectrpl/main.php
+++ projectrpl/main.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="perpuslogin.css">
     <title>Perpustakaan Digital - Login</title>
 </head>
+
 <body>
     <div class="container">
         <form class="login-form">
@@ -24,9 +26,9 @@
         </form>
 
         <div class="register-link">
-            Belum punya akun? <a href="register.html">Daftar disini</a>
+            Belum punya akun? <a href="../projectrpl/register.html">Daftar disini</a>
         </div>
-        <div>tefsfsdhbsuyfyuuasfs</div>
     </div>
 </body>
+
 </html>
